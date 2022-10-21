0e2d2184e3778a41744486ef207599109d9858fb
theseus27 <theseus.lim@gmail.com>
2022-10-21 10:53:53 -0400

    reorganization

--- pokemon_drafts/draft 1/frontend/src/pages/error-page/index.jsx
+++
@@ -1,6 +0,0 @@
-
-const ErrorPage = () => {
-  
-}
-
-export default ErrorPage
--- pokemon_drafts/fast_react/frontend/src/utils/query_function.jsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-import { QueryFunction } from "react-query"
-
-export const query_function = async ({ queryKey }) => {
-  const { data } = await axios.get(`http://localhost:8000/${queryKey[0]}`);
-  var parsed = JSON.parse(data)
-  return parsed;
-}
