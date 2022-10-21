01b017737e7ad16f936129132f38bff0fdeb2cd2
theseus27 <theseus.lim@gmail.com>
2022-10-21 10:58:20 -0400

    reorgnization

--- scraping/tempCodeRunnerFile.py
+++
@@ -1,5 +0,0 @@
-    # if post.find("username") == author:
-    #     paragraphs = post.find_all("p")
-    #     for p in paragraphs:
-    #         print(p.contents)
-    # print("---------------------------------------")
